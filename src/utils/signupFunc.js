2f99090774e0cf948e121ce37a6bc0d48ff8a941
Julius Adebowale <juadebgabriel@gmail.com>
2025-01-25 12:58:27 +0100

    Feat: Implemented the signup functionality

--- src/utils/signupFunc.js
+++ src/utils/signupFunc.js
@@ -1,9 +1,12 @@
+console.log("We are here");
+
 //get the form elements
 const signupForm = document.getElementById("signup_form");
 const signupButton = document.getElementById("signup_button");
 
 // input fields
 const username = document.getElementById("username");
+console.log(username.value);
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -21,7 +24,7 @@ const confirmPasswordError = document.getElementById("confirm_password_error");
 //utility functions
 // check usernname validity
 function isValidUsername(username) {
-  return /^[a-zA-Z0-9]{3,30}$/.test(username);
+  return /^[a-zA-Z0-9]{3,30}$/.test(username); // false or true
 }
 
 //checks validity for phone numbers
@@ -52,16 +55,17 @@ function getPasswordStrength(password) {
   ) {
     return "Medium";
   }
+
   return "Weak";
 }
 
 function arePasswordMatching(password, confirmPassword) {
-  return password === confirmPassword;
+  return password === confirmPassword; //true /false
 }
 
 //validation logic proper
 function validateInputs() {
-  let isValid = true;
+  let isValid = true; // is to help with ennabling the signup button
 
   //validate username
   if (!isValidUsername(username.value)) {
@@ -87,11 +91,74 @@ function validateInputs() {
     emailError.classList.add("hidden");
   }
 
-  //validate password
-  if (!isValidEmail(email.value)) {
-    emailError.classList.remove("hidden");
-    isValid = false;
+  //validate password strength
+  const strength = getPasswordStrength(password.value);
+  strengthIndicator.textContent = strength;
+
+  if (strength === "Weak") {
+    passwordError.classList.remove("hidden");
   } else {
-    emailError.classList.add("hidden");
+    passwordError.classList.add("hidden");
   }
+
+  //confirm password against password
+  if (!arePasswordMatching(password.value, confirmPassword.value)) {
+    confirmPasswordError.classList.remove("hidden");
+  } else {
+    confirmPasswordError.classList.add("hidden");
+  }
+
+  //enable signup button
+  signupButton.disabled = !isValid;
+  signupButton.classList.toggle("cursor-not-allowed", !isValid);
+  signupButton.classList.toggle("bg-disabled_btn_bg", !isValid);
+  signupButton.classList.add("bg-main_blue", !isValid);
 }
+
+// adding events to the individual inputs
+username.addEventListener("input", validateInputs);
+phone.addEventListener("input", validateInputs);
+email.addEventListener("input", validateInputs);
+password.addEventListener("input", validateInputs);
+confirmPassword.addEventListener("input", validateInputs);
+
+//form submission
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // gather data
+  const signupData = {
+    username: username.value.trim(),
+    phone: phone.value.trim(),
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  const reqObj = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(signupData), // coverting it to json obj
+  };
+
+  // the url
+  const url = "http://localhost:3002/api/auth/signup";
+
+  //send to backend
+  fetch(url, reqObj)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Signup Failed");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      alert("Signup Successful");
+      window.location.href = "./dashboard.html";
+      signupForm.reset(); //cleans all user input from the form
+      validateInputs(); //resets validation state
+    })
+    .catch((error) => {
+      console.log(error);
+      alert(error.message);
+    });
+});
